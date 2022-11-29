0cdc5694c19f730c66ef8ae7dbd9a9b41faa37e4
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-11-29 02:40:41 -0600

    started on capstone working on login and nav workings

---
+++ src/apiKeys.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaM2aE6iEOO7isKlrpsKFzc43mKZVrZGs",
+  authDomain: "yawa-206cf.firebaseapp.com",
+  projectId: "yawa-206cf",
+  storageBucket: "yawa-206cf.appspot.com",
+  messagingSenderId: "137096588044",
+  appId: "1:137096588044:web:63668f290f895aa5cdf729"
+};
